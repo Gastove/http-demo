3164d09f21b5deaf7c20385e9f61671b1da26276
Ross Donaldson <gastove@gmail.com>
2016-04-23 12:16:13 -0700

    Clean this up for the lesson
    Doc strings, a few more utility methods. Hot dog!

--- http-demo/main.py
+++ http-demo/main.py
@@ -65,16 +65,20 @@ def yousaid():
     return render_template("bevchoice.html", bev=choice)
 
 
-# Utility method for creating the tables in the database
+# Utility method for creating all the tables in the database
+@app.route('/admin/bootstrap')
 def bootstrap():
     schema.Base.metadata.create_all(db.engine)
+    return "Bootsrapped!"
 
 
+# This form allows us to add a new person to the database.
 @app.route('/addperson')
 def add_a_person():
     return render_template('new_person_form.html')
 
 
+# Here is the actual method that receives our new person form.
 @app.route('/newperson', methods=['POST'])
 def add_person():
     session = db.get_session()
@@ -85,14 +89,25 @@ def add_person():
     session.add(new_person)
     session.commit()
 
-    return 'Okay maaaade a person!'
+    return 'Okay maaaade a person! {} {} has been saved'.format(
+        new_person.first_name,
+        new_person.last_name
+    )
 
 
+# This route allows us to list people from the database. With no params, we list
+# everyone; with an id resource, we list on the person who matches that id.
 @app.route('/listpeople')
-def list_people():
+@app.route('/listpeople/<id>')
+def list_people(id=None):
     session = db.get_session()
-    res = session.query(schema.Person).all()
-    return res
+
+    if id is None:
+        people = session.query(schema.Person).all()
+    else:
+        people = session.query(schema.Person).filter(schema.Person.id == id)
+    return render_template("person.tpl", people=people)
+
 
 if __name__ == '__main__':
     app.run()
